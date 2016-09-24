ead049f91ec522968223e72b51e172fbfd25ddf1
Édouard WILLISSECK <edouard.willisseck@gmail.com>
2016-09-24 13:31:01 +0200

    Added "index.html", favicons and materialize.

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Coq'tail</title>
+        <?php include_once("favicon.inc.php"); ?>
+    </head>
+    <body>
+
+    </body>
+</html>
