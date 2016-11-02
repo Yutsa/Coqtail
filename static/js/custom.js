7843120c9d7990cddc0eaafb5819849ca97ab837
Édouard WILLISSECK <edouard.willisseck@gmail.com>
2016-11-02 09:45:52 +0100

    The ingredient list is now displayed but still bugged.

--- static/js/custom.js
+++ static/js/custom.js
@@ -13,28 +13,27 @@ $(".button-collapse").sideNav();
 
 $('body').on('click', 'div.collapsible-header', function() {
     var url = 'core/ajax.php';
-    
+
     var nextCollapsible = $(this).next();
     var categorie = $(this).text();
-    
-    console.log.($(this).parent().parent().prop('tagName'));
-    
-    $(this).parent().parent().addClass('blue');
-    
+
+    //console.log.($(this).parent().parent().prop('tagName'));
+
+    //$(this).parent().parent().addClass('blue');
+
     $.post(url, { cat : categorie }, function(data) {
         //nextCollapsible.empty();
         nextCollapsible.html(data);
-        
+
         //console.log(nextCollapsible.children().text());
         //console.log(nextCollapsible.children().prop('tagName'));
-        
+
         nextCollapsible.children().collapsible({
             accordion: true
         });
-        
+
         //$(this).addClass('active');
-        
+
         //alert(data);
     });
 });
-
