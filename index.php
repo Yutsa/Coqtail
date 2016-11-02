a93e579276f2336d78d5f74142a7d5d844d7850d
Édouard WILLISSECK <edouard.willisseck@gmail.com>
2016-11-02 11:09:17 +0100

    Finally fixed the merge bugs.

--- index.php
+++ index.php
@@ -38,14 +38,10 @@ include_once("core/functions.inc.php");
             ?>
         </main>
 
+
         <script type="text/javascript" src="static/js/jquery-3.1.0.min.js"></script>
         <script type="text/javascript" src="static/js/materialize.min.js"></script>
         <script src="static/js/custom.js" charset="utf-8"></script>
-    </body>
-
-    <script type="text/javascript" src="static/js/jquery-3.1.0.min.js"></script>
-    <script type="text/javascript" src="static/js/materialize.min.js"></script>
-    <script src="static/js/custom.js" charset="utf-8"></script>
-    <script src="http://localhost:35729/livereload.js" charset="utf-8"></script>
+        <script src="http://localhost:35729/livereload.js" charset="utf-8"></script>
     </body>
 </html>
