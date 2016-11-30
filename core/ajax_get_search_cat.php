db93fdd620da08781dc961e5dd10249598a3cc7c
Thibert LETULLIER <thibert57@me.com>
2016-11-30 23:37:11 +0100

    Begin rapport, change for approximative search

--- core/ajax_get_search_cat.php
+++ core/ajax_get_search_cat.php
@@ -34,6 +34,8 @@ function isWithIngredient(&$Hierarchie, $searchIngredient, $recette)
     return false;
 }
 
+$atLeastOneRecite = false;
+$suggestRecite = array();
 
 //Iterates through each recites
 foreach($Recettes as $recette)
@@ -79,10 +81,41 @@ foreach($Recettes as $recette)
         }
     }
 
-    //If there are all ingredient need and all ingredient don't need
-    if ($okWith == $nbWith && $okWithout == $nbWithout && ($nbWithout > 0 || $nbWith > 0))
+    //If there are at least one ingredient to search
+    if ($nbWithout > 0 || $nbWith > 0)
     {
-        displayCocktail($recette);
+        //If there are all ingredient need and all ingredient don't need
+        if ($okWith == $nbWith 
+            && $okWithout == $nbWithout)
+        {
+            //Display recites matchs
+            displayCocktail($recette);
+            $atLeastOneRecite = true;
+        }
+        //Else, try with a recite with 1 ingredient doesn't match
+        else if ($okWith == $nbWith-1)
+        {
+            $suggestRecite[] = $recette;
+        }
+    }
+}
+
+//If there are no recite display (no match)
+if (!$atLeastOneRecite && ($nbWithout > 0 || $nbWith > 0))
+{
+    //If there is at least one approximative recite, display it
+    if (count($suggestRecite) > 0)
+    {
+        echo ("<p class='red'>Aucune recette n'existe avec ces ingredients, voici des recettes dont le résultat est proche :");
+        foreach($suggestRecite as $recite)
+        {
+            displayCocktail($recite);
+        }
+    }
+    //If there is no recite, display an error message
+    else 
+    {
+        echo ("<p class='red'>Aucunes recette n'existe avec ces ingredients, ni aucunes recherche approximative");
     }
 }
 ?>
