36bb16d8b81c2a94fc139de430be0d277ca83329
Thibert LETULLIER <thibert57@me.com>
2016-12-05 18:03:18 +0100

    final version ?

--- templates/profil.php
+++ templates/profil.php
@@ -2,47 +2,112 @@
 
 include_once("../core/functions.inc.php");
 include_once("../core/donnees.inc.php");
-//define("indexFilePath", "../data/accounts_index");
-//session_start();
-
+include_once("../core/functions_user.php");
 
 if(isConnected())
 {
+    //Initialize error variable
+    if (!isset($mailError)) $mailError = '';
+    if (!isset($passwordError)) $passwordError = '';
+
+    if (!isset($nameError)) $nameError = '';
+    if (!isset($firstNameError)) $firstNameError = '';
+
+    if (!isset($phoneError)) $phoneError = '';
+    if (!isset($postalError)) $postalError = '';
+    if (!isset($villeError)) $villeError = '';
+    if (!isset($passwordError)) $passwordError = '';
+    if (!isset($newPasswordError)) $newPasswordError = '';
     $change = '';
 
+    //Initialize paths
     $userDataFileName = $_SESSION["userDataFileName"];
     $userDataFilePath = "../data/" . $userDataFileName;
     $userDataFile = fopen($userDataFilePath, "a+");
     $userData = unserialize(fgets($userDataFile));
-    //print_r($userData);
 
     $email = $userData["email"];
     $password = $userData["password"];
 
+    //If Post is send for 1st form
     if(isset($_POST["firstname"]))
     {
-        //Change with new value
-        $userData["firstname"] = $_POST["firstname"];
-        $userData["name"] = $_POST["name"];
-        $userData["ville"] = $_POST["ville"];
-        $userData["phone"] = $_POST["phone"];
-        $userData["postal"] = $_POST["postal"];
-        $userData["address"] = $_POST["address"];
-        $userData["naissance"] = $_POST["naissance"];
-        $userData["sexe"] = $_POST["sexe"];
+        //Check all input of form
+        if (testName($_POST["name"], $nameError))
+            $hasError = true;
 
-        $userDataFileCopy = fopen($userDataFilePath . "copy", "a+");
-        fwrite($userDataFileCopy, serialize($userData));
-        unlink($userDataFilePath);
+        if (testFirstName($_POST["firstname"], $firstNameError))
+            $hasError = true;
 
-        copy($userDataFilePath . "copy", $userDataFilePath);
-        unlink($userDataFilePath . "copy");
+        if (testVille($_POST["ville"], $villeError))
+            $hasError = true;
 
-        $change = 'Vos infomations ont bien été changées.';
+        if (testPostal($_POST["postal"], $postalError))
+            $hasError = true;
+
+        if (testPhone($_POST["phone"], $phoneError))
+            $hasError = true;
+
+        //If there is no error 
+        if (!$hasError)
+        {
+            //Change userdata with new value
+            $userData["firstname"] = $_POST["firstname"];
+            $userData["name"] = $_POST["name"];
+            $userData["ville"] = $_POST["ville"];
+            $userData["phone"] = $_POST["phone"];
+            $userData["postal"] = $_POST["postal"];
+            $userData["address"] = $_POST["address"];
+            $userData["naissance"] = $_POST["naissance"];
+            $userData["sexe"] = $_POST["sexe"];
+            //Message display
+            $change = 'Vos infomations ont bien été changées.';
+        }
     }
+
+    //If post is send for 2nd form
+    if(isset($_POST["password"]) && isset($_POST["newpassword"]))
+    {
+        //If input are full
+        if (!empty($_POST["password"]) && !empty($_POST["newpassword"]))
+        {
+            //Verify if actual password is good
+            if (password_verify($_POST["password"], $userData["password"]))
+            {
+                //Change with new password
+                $newPassword = password_hash($_POST["newpassword"], PASSWORD_BCRYPT);
+                $userData["password"] = $newPassword;
+                $change = 'Vos infomations ont bien été changées.';
+            }
+            else 
+            {
+                $passwordError = "Mot de passe invalide";
+            }
+        }
+        else
+        {
+            if (empty($_POST["password"]))
+            {
+                $passwordError = 'Mot de passe requis';
+            }
+            if (empty($_POST["newpassword"]))
+            {
+                $newPasswordError = 'Nouveau mot de passe requis';
+            }
+        }
+    }
+
+    //Copy the new informations in the save file
+    $userDataFileCopy = fopen($userDataFilePath . "copy", "a+");
+    fwrite($userDataFileCopy, serialize($userData));
+    unlink($userDataFilePath);
+
+    copy($userDataFilePath . "copy", $userDataFilePath);
+    unlink($userDataFilePath . "copy");
+
     fclose($userDataFile);
 
-
+    //Initaliaze the fields for input
     $firstname = '';
     $name = '';
     $phone = '';
@@ -130,10 +195,12 @@ else
                         <div class="input-field col s4">
                             <p for="name">Nom : </p>
                             <input id="name" name="name" type="text" class="validate" value="<?php echo $name ?>">
+                            <span style="color:red"><?= $nameError ?> </span>
                         </div>
                         <div class="input-field col s4">
-                            <p for="firstname">Prénom : </p>
+                            <p for="nom">Prénom : </p>
                             <input id="firstname" name="firstname" type="text" class="validate" value="<?php echo $firstname ?>" />
+                            <span style="color:red"><?= $firstNameError ?> </span>
                         </div>
                         <div class="input-field col s4">
                             <p>
@@ -152,6 +219,7 @@ else
                         <div class="input-field col s6">
                             <p>Téléphone : </p>
                             <input type="text" name="phone" id="phone" value="<?php echo $phone ?>"/>
+                            <span style="color:red"><?= $phoneError ?> </span>
                         </div>
                     </div>
                     <div class="row">
@@ -162,10 +230,12 @@ else
                         <div class="input-field col s4">
                             <p for="nom">Code postal : </p>
                             <input type="text" name="postal" id="postal" value="<?php echo $postal ?>"/>
+                            <span style="color:red"><?= $postalError ?> </span>
                         </div>
                         <div class="input-field col s4">
                             <p for="nom">Ville : </p>
                             <input type="text" name="ville" id="ville" value="<?php echo $ville ?>"/>
+                            <span style="color:red"><?= $villeError ?> </span>
                         </div>
                     </div>
                     <div class="row">
@@ -184,10 +254,12 @@ else
                     <div class="input-field col s6">
                         <input id="password" name="password" type="password" class="validate" required="" aria-required="true">
                         <label for="password">Mot de passe</label>
+                        <span style="color:red"><?= $passwordError ?> </span>
                     </div>
                     <div class="input-field col s6">
-                        <input id="newPassword" name="newPassword" type="password" class="validate" required="" aria-required="true">
-                        <label for="newPassword">Nouveau mot de passe</label>
+                        <input id="newpassword" name="newpassword" type="password" class="validate" required="" aria-required="true">
+                        <label for="newpassword">Nouveau mot de passe</label>
+                        <span style="color:red"><?= $newPasswordError ?> </span>
                     </div>
                     <div class="row">
                         <button class="btn waves-effect waves-light col s4 offset-s4 " type="submit" name="action">
@@ -197,7 +269,6 @@ else
                     </div>
                 </form>
             </div>
-
         </div>
 
         <script type="text/javascript" src="../static/js/jquery-3.1.0.min.js"></script>
